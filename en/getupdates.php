ad31f367c64e51751222812c782177cfcfc6289e
Ge Jian Feng <gejianfeng2@126.com>
2018-04-18 01:02:06 +0800

    + update

--- en/getupdates.php
+++ en/getupdates.php
@@ -6,6 +6,28 @@ Template Name: getupdates.php
 
 <?php get_header(); ?>
 
-<?php get_tbc_template(); ?>
+<div id="getupdates">
+	<div class="bg-wrap">
+		<div class="background">
+			<div class="wrap">
+				<div class="frame">
+					<div class="frame-title main" style="top: 40px; left: 30px;">Subscribe</div>
+
+					<div class="field-title main" style="top: 130px; left: 30px;">Name</div>
+					<input id="fname" class="input-box" type="text" style="left: 30px; top: 170px; position:absolute; width: 292px; height: 44px"/>
+					<input id="lname" class="input-box" type="text" style="left: 335px; top: 170px; position:absolute; width: 292px; height: 44px"/>
+
+					<div class="field-title sub" style="top: 220px; left: 30px;">First Name</div>
+					<div class="field-title sub" style="top: 220px; left: 335px;">Last Name</div>
+
+					<div class="field-title main" style="top: 250px; left: 30px;">Email Address</div>
+					<input id="mail" class="input-box" type="text" style="left: 30px; top: 290px; position:absolute; width: 594px; height: 44px"/>
+
+					<div class="button">SUBMIT</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php get_footer(); ?>
