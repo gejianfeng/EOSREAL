a1d3958dcef43491737668959852fc576abddff7
Ge Jian Feng <gejianfeng2@126.com>
2018-04-17 23:17:08 +0800

    + add

--- en/team.php
+++ en/team.php
@@ -6,6 +6,47 @@ Template Name: team.php
 
 <?php get_header(); ?>
 
-<?php get_tbc_template(); ?>
+<div id="team">
+	<div class="wrap">
+		<div class="container">
+			<div class="title">our team</div>
+			<div class="block" style="margin-top: 73px !important;">
+				<div class="photo"></div>
+				<div class="name">Yifeng Mao</div>
+				<div class="line"></div>
+				<div class="position">Partner</div>
+				<div class="intro">CFA, Founder, Head of Research, has developed investment strategies and institution both on the buy and sell sides. He has managed global hedge funds in Singapore and Hong Kong as well as setting up a family office in the United Staes. Yifeng is considered a pre-eminent name in the data intensive bottoms-up investment research arena with particular stree in Chinese listed companies. He is a featured speaker on international business channels which solicit his indigenously developed proprietary cutting edge analysis techniques which have proven to be non-consensus and highly accurate. He graduated from Shanghai Jiao Tong University with dual undergraduate degrees in Computer Science and Finance.</div>
+			</div>
+			<div class="block">
+				<div class="photo"></div>
+				<div class="name">Yifeng Mao</div>
+				<div class="line"></div>
+				<div class="position">Partner</div>
+				<div class="intro">CFA, Founder, Head of Research, has developed investment strategies and institution both on the buy and sell sides. He has managed global hedge funds in Singapore and Hong Kong as well as setting up a family office in the United Staes. Yifeng is considered a pre-eminent name in the data intensive bottoms-up investment research arena with particular stree in Chinese listed companies. He is a featured speaker on international business channels which solicit his indigenously developed proprietary cutting edge analysis techniques which have proven to be non-consensus and highly accurate. He graduated from Shanghai Jiao Tong University with dual undergraduate degrees in Computer Science and Finance.</div>
+			</div>
+			<div class="block">
+				<div class="photo"></div>
+				<div class="name">Yifeng Mao</div>
+				<div class="line"></div>
+				<div class="position">Partner</div>
+				<div class="intro">CFA, Founder, Head of Research, has developed investment strategies and institution both on the buy and sell sides. He has managed global hedge funds in Singapore and Hong Kong as well as setting up a family office in the United Staes. Yifeng is considered a pre-eminent name in the data intensive bottoms-up investment research arena with particular stree in Chinese listed companies. He is a featured speaker on international business channels which solicit his indigenously developed proprietary cutting edge analysis techniques which have proven to be non-consensus and highly accurate. He graduated from Shanghai Jiao Tong University with dual undergraduate degrees in Computer Science and Finance.</div>
+			</div>
+			<div class="block">
+				<div class="photo"></div>
+				<div class="name">Yifeng Mao</div>
+				<div class="line"></div>
+				<div class="position">Partner</div>
+				<div class="intro">CFA, Founder, Head of Research, has developed investment strategies and institution both on the buy and sell sides. He has managed global hedge funds in Singapore and Hong Kong as well as setting up a family office in the United Staes. Yifeng is considered a pre-eminent name in the data intensive bottoms-up investment research arena with particular stree in Chinese listed companies. He is a featured speaker on international business channels which solicit his indigenously developed proprietary cutting edge analysis techniques which have proven to be non-consensus and highly accurate. He graduated from Shanghai Jiao Tong University with dual undergraduate degrees in Computer Science and Finance.</div>
+			</div>
+			<div class="block">
+				<div class="photo"></div>
+				<div class="name">Yifeng Mao</div>
+				<div class="line"></div>
+				<div class="position">Partner</div>
+				<div class="intro">CFA, Founder, Head of Research, has developed investment strategies and institution both on the buy and sell sides. He has managed global hedge funds in Singapore and Hong Kong as well as setting up a family office in the United Staes. Yifeng is considered a pre-eminent name in the data intensive bottoms-up investment research arena with particular stree in Chinese listed companies. He is a featured speaker on international business channels which solicit his indigenously developed proprietary cutting edge analysis techniques which have proven to be non-consensus and highly accurate. He graduated from Shanghai Jiao Tong University with dual undergraduate degrees in Computer Science and Finance.</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php get_footer(); ?>
