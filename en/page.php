a8cf4bbe32283efb5e1b3a9c2ae637fee5996cc4
Ge Jian Feng <gejianfeng2@126.com>
2018-04-19 01:10:51 +0800

    + add

---
+++ en/page.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: page.php
+ */
+?>
+
+<?php get_header(); ?>
+
+<?php get_tbc_template(); ?>
+
+<?php get_footer(); ?>
---
+++ en/single.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: single.php
+ */
+?>
+
+<?php get_header(); ?>
+
+<?php get_tbc_template(); ?>
+
+<?php get_footer(); ?>
