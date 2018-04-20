36e65bfe7cbbe8f82085fec385b538e7b7743b2b
Ge Jian Feng <gejianfeng2@126.com>
2018-04-20 13:57:34 +0800

    + update

--- en/news.php
+++ en/news.php
@@ -19,13 +19,36 @@ Template Name: news.php
 			{
 				$bFound = true;
 				
-				$news_query->the_post();
-				echo $post->post_title;
+				/* page body with article*/
+				echo '<div id="news">';
+				echo '<div class="wrap">';
+				echo '<div class="container">';
+				echo '<div class="article-title">' . $post->post_title . '</div>';
+				echo '<div class="article-date">';
+				echo date('M', get_the_time('U'));
+				the_time(' jS Y');
+				echo '</div>';
+				echo '<div class="article-content">';
+				echo $post->post_content;
+				echo '</div>';
+				echo '</div>';
+				echo '</div>';
+				echo '</div>';
 
 				break;
 			}
 		endwhile;
 	endif;
+
+	if ($bFound == false)
+	{
+		/* page body for no article */
+		echo '<div id="tbc">';
+		echo '<div class="inner-wrap">';
+		echo '<div class="content">Article Not Found</div>';
+		echo '</div>';
+		echo '</div>';
+	}
 
 	wp_reset_query();
 ?>
