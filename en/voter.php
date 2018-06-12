95bda34c0e1478c2d61054a76be3bc19a680dfe5
Ge Jian Feng <gejianfeng2@126.com>
2018-06-12 12:59:42 +0800

    + update

--- en/voter.php
+++ en/voter.php
@@ -33,12 +33,12 @@ Template Name: voter.php
 				<div class="title-container">
 					<div class="title1">EOS Voter</div>
 					<div class="title-line"></div>
-					<div class="title2">目前连接到**eos主网**网络。<br/>链ID=aca 376f 206b 8fc 25a 6ed 44dbdc 66547c 36c 363c 33a 119ffbef 943642f 0e906。<br/>参加选举的选举委员比例1.32%<br/>13,177,943eos投票支持启动该链所需的150,000,000 
+					<div class="title2">目前连接到**eos主网**网络。<br/>链ID=aca 376f 206b 8fc 25a 6ed 44dbdc 66547c 36c 363c 33a 119ffbef 943642f 0e906。<br/><span id="total_voter"></span>eos投票支持启动该链 
 					</div>
 					<div class="title-button" onclick="showVoterSite();">投票门户列表</div>
 				</div>
 
-				<div class="search-container">
+				<div class="search-container" style="display: none;">
 					<div class="search-title">搜索</div>
 					<div class="search-icon"></div>
 				</div>
@@ -52,7 +52,7 @@ Template Name: voter.php
 					<div class="context-sub-title-container">
 						<div class="context-sub-title" style="left: 205px;">超级节点</div>
 						<div class="context-sub-title" style="left: 855px;">目前总投票数</div>
-						<div class="context-sub-title" style="left: 1205px;">国家</div>
+						<!--<div class="context-sub-title" style="left: 1205px;">国家</div>-->
 						<div class="context-sub-title" style="left: 1400px;">信息</div>
 					</div>
 
@@ -99,6 +99,10 @@ window.onload=function (){
 	}
 
 	var total = +data.total_producer_vote_weight;
+
+	var total_formatted = number_format(total, 0, '.', ',');
+	$("#total_voter").html(total_formatted);
+
 	var count = data.rows.length;
 
 	var htmlCode = "";
@@ -109,8 +113,9 @@ window.onload=function (){
 		var name = o.owner;
 		var url = o.url;
 		var val = +o.total_votes;
-		var val1 = val / 1000000;
+		var val1 = val / 3719520000;
 		val1 = val1.toFixed(2);
+		val1 += 'M';
 		var val2 = val / total * 100;
 		val2 = val2.toFixed(2);
 		val2 = val2.toString() + "%";
@@ -118,13 +123,45 @@ window.onload=function (){
 		htmlCode += "<div class='dynamic-container'>";
 		htmlCode += "<div class='dynamic-text' style='left: 26px;'>" + idx + "</div>";
 		htmlCode += "<div class='dynamic-text' style='left: 185px;'>" + name + "</div>";
-		htmlCode += "<div class='dynamic-text' style='left: 835px;'>" + val2 + "</div>";
-		htmlCode += "<div class='dynamic-text' style='left: 1185px;'>KR</div>";
+		htmlCode += "<div class='dynamic-text' style='left: 835px;'>" + "<span style='color: rgb(198, 125, 11);'>" + val1 + "</span> " + val2 + "</div>";
+		//htmlCode += "<div class='dynamic-text' style='left: 1185px;'>KR</div>";
 		htmlCode += "<div class='dynamic-text' style='left: 1380px;'>" + url + "</div>";
 		htmlCode += "</div>";
 	}
 
 	$("#dynamic").html(htmlCode);
+}
+
+function number_format(number, decimals, dec_point, thousands_sep) {
+    /*
+    * 参数说明：
+    * number：要格式化的数字
+    * decimals：保留几位小数
+    * dec_point：小数点符号
+    * thousands_sep：千分位符号
+    * */
+    number = (number + '').replace(/[^0-9+-Ee.]/g, '');
+    var n = !isFinite(+number) ? 0 : +number,
+        prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+        sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+        dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+        s = '',
+        toFixedFix = function (n, prec) {
+            var k = Math.pow(10, prec);
+            return '' + Math.ceil(n * k) / k;
+        };
+ 
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+    var re = /(-?\d+)(\d{3})/;
+    while (re.test(s[0])) {
+        s[0] = s[0].replace(re, "$1" + sep + "$2");
+    }
+ 
+    if ((s[1] || '').length < prec) {
+        s[1] = s[1] || '';
+        s[1] += new Array(prec - s[1].length + 1).join('0');
+    }
+    return s.join(dec);
 }
 
 </script>
